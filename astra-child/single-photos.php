3d8e87c059bfb2b94e5e0931105a2258e74a6c05
Margot Dubedat <margot.dubedat.pro@gmail.com>
2024-04-07 11:59:56 +0200

    création du single-photos.php vierge

---
+++ astra-child/single-photos.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<div id="primary">
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
